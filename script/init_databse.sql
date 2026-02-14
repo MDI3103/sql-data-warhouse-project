c74a5147072d78b29cf04111fe1723b8efd93602
Muhammad Danish Irfan Bin MD Zamri <m.danishirfan78@gmail.com>
2026-02-14 23:31:49 +0800

    Update init_databse.sql

--- script/init_databse.sql
+++ script/init_databse.sql
@@ -1,1 +1,35 @@
+/*
+Create Database and Schemas
+Schema=bronze,silver,gold
+
+WARNING:
+Running this script will drop the entire 'DataWarehouse' database if it exist.
+All data in the database will be permently deleted.Proceed with cautoon and ensure you
+have proper backup before running this script
+*/
+
+
+
+USE master;
+GO
+
+--Drop and recreate the DataWarehouse database
+IF EXISTS(SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+  --will alter the database and then set to only single user and then remove any other user
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE Datawarehouse;
+END;
+
+--Create DataWarehouse DB
+CREATE Database DataWarehouse;
+
+USE DataWarehouse;
+
+--Create Schema
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
 
